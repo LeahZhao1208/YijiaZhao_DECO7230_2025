79410b0228ff8ba0d3a56e6652d04636273e4f65
LeahZhao1208 <yijia.zhao1208@gmail.com>
2025-09-19 00:29:12 +1000

    Prototype2 Draft1

---
+++ Prototype2_Yijia/Zoom VR/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public DebugRotator targetRotator;
+    public Button toggleButton;
+
+    void Start()
+    {
+        toggleButton.onClick.AddListener(ToggleRotation);
+    }
+
+    void ToggleRotation()
+    {
+        targetRotator.isRotating = !targetRotator.isRotating;
+    }
+}
